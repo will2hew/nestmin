0cd25a93235f5ad563a74d06462f01d411eabbc0
Will Hewitt <will@hew.nz>
2024-08-18 18:16:11 +1200

    wip

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,10 +1,9 @@
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
-import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   server: {
     proxy: {
       "/api": {
